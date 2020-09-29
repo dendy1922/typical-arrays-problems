6aa8fdd78183d92021389268675bd74bfbd4d35f
Artsiom Dragun <dragun1922@gmail.com>
2020-09-29 07:34:15 +0300

    feat: made functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array) {
+    return array.sort(compare)[0] || [];
+  }
+  return []
+}
+
+
+exports.max = function max(array) {
+  if (array) {
+    return array.sort(compare)[array.length - 1] || [];
+  }
+  return []
+}
+
+exports.avg = function avg(array) {
+  if (array) {
+    return (array.reduce((a, b) => a + b, 0) / (array.length || 1)).toFixed(2);
+  }
   return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+function compare(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
 }
